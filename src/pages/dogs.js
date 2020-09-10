749455b8926937e6180d13fbfb1f5eaf248e2c83
Jørgen Hellemann <jorgen.hellemann@makingwaves.no>
2020-09-10 15:28:56 +0200

    build time vs runtime data test page

--- src/pages/dogs.js
+++ src/pages/dogs.js
@@ -1,18 +1,36 @@
-import React from "react"
-import { Link, graphql } from "gatsby"
+import React, { useState, useEffect } from "react"
+import { Link, useStaticQuery } from "gatsby"
 
-export default ({ data }) => (
-  <React.Fragment>
-    <img src={data.dogs.message} alt="a cute dog" />
-    <br />
-    <Link to="/">Back to home</Link>
-  </React.Fragment>
-)
-
-export const query = graphql`
-  query {
-    dogs {
-      message
+const DogsPage = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      dogs {
+        message
+      }
     }
-  }
-`
+  `)
+
+  // Client-side Runtime Data Fetching
+  const [randomDog, setRandomDog] = useState(0)
+  useEffect(() => {
+    fetch(`https://dog.ceo/api/breeds/image/random`)
+      .then(response => response.json())
+      .then(resultData => {
+        setRandomDog(resultData.message)
+      })
+  }, [])
+
+  return (
+    <React.Fragment>
+      <h4>This is a static dog:</h4>
+      <img src={data.dogs.message} alt="a cute static dog" />
+      <br />
+      <h4>This is a random dog:</h4>
+      <img src={randomDog} alt="a cute random dog" />
+      <br />
+      <Link to="/">Back to home</Link>
+    </React.Fragment>
+  )
+}
+
+export default DogsPage
