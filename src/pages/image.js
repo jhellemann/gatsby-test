26860b3305f0afdd22a7a4e72b747ffe1af32b3a
Jørgen Hellemann <jorgen.hellemann@makingwaves.no>
2020-09-14 12:40:49 +0200

    added image

---
+++ src/pages/image.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import ImageAstronaut from "../components/image-astronaut"
+
+const ImagePage = () => (
+  <Layout>
+    <div style={{ maxWidth: `600px`, marginBottom: `1.45rem` }}>
+      <ImageAstronaut />
+    </div>
+  </Layout>
+)
+
+export default ImagePage
