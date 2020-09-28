ff426973e133b1ed9ead8b481f7c1cd9237b8e6f
Jørgen Hellemann <jorgen.hellemann@makingwaves.no>
2020-09-28 10:25:50 +0200

    added .net api

---
+++ api/StarWarsFunction.cs
@@ -0,0 +1,35 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+
+namespace Api
+{
+    public static class StarWarsFunction
+    {
+        [FunctionName("star-wars")]
+        public static IActionResult Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            var data = new List<string>
+            {
+                "Episode I – The Phantom Menace",
+                "Episode II – Attack of the Clones",
+                "Episode III – Revenge of the Sith",
+                "Episode IV – A New Hope",
+                "Episode V – The Empire Strikes Back",
+                "Episode VI – Return of the Jedi",
+                "Episode VII – The Force Awakens",
+                "Episode VIII – The Last Jedi",
+                "Episode IX – The Rise of Skywalker"
+            };
+
+            return new OkObjectResult(data);
+        }
+    }
+}
